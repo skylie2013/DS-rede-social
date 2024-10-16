03f34d2c2e6b455ccc59e177473d3036e506f641
skylie2013 <104508830+skylie2013@users.noreply.github.com>
2024-10-16 07:50:33 -0300

    aula 10

--- graficos/common.js
+++ graficos/common.js
@@ -1,5 +1,12 @@
 const getCSS = (variavel) => {
+  const bodyStyles = getComputedStyle(document.body)
   return getComputedStyle(document.body).getPropertyValue(variavel)
-};
+}
 
-export { getCSS }
+const tickConfig = {
+  color: getCSS('--primary-color'),
+  size: 16,
+  family: getCSS('--font')
+}
+
+export { getCSS, tickConfig }
